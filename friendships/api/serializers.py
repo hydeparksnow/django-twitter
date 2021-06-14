266343864398887c978e0c90ea506eafc51dce24
yingzhao <yingzhaochicago@gmail.com>
2021-06-13 23:12:36 -0700

    add follow and unfollow

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from accounts.api.serializers import UserSerializerForFriendship
 from friendships.models import Friendship
 from rest_framework import serializers
@@ -19,3 +21,29 @@ class FollowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friendship
         fields = ('user', 'created_at')
+
+class FriendshipSerializerForCreate(serializers.ModelSerializer):
+    from_user_id = serializers.IntegerField()
+    to_user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Friendship
+        fields = ('from_user_id', 'to_user_id')
+
+    def validate(self, attrs):
+        if attrs['from_user_id'] == attrs['to_user_id']:
+            raise ValidationError({
+                'message': 'You can not follow yourself',
+            })
+        if not User.objects.filter(id=attrs['to_user_id']).exists():
+            raise ValidationError({
+                'message': 'You can not follow non-exist user.'
+            })
+
+        return attrs
+
+    def create(self, validated_data):
+        return Friendship.objects.create(
+            from_user_id=validated_data['from_user_id'],
+            to_user_id=validated_data['to_user_id'],
+        )
